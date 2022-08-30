03b464fb49ff1ccf7b85c42fccd4a9a5ec46221a
madhums0411 <madhums41175@gmail.com>
2022-08-30 22:50:41 +0530

    Create Address book table

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,3 +1,18 @@
 ----UC1 Create database
 Create database AddressBook_Service
 use AddressBook_Service
+
+----UC2 Create Addres Book table
+
+create table AddressBook
+(
+FirstName varchar(50) not null,
+LastName varchar(50) not null,
+Address varchar(100) not null,
+City varchar(50) not null,
+State varchar(50) not null,
+Zipcode varchar(50) not null,
+PhoneNumber varchar(50) not null,
+Email varchar(50) not null
+)
+Select * from AddressBook
